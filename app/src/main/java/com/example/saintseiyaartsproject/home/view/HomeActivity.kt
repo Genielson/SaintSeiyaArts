b612672c5fd5cb9a4350a18b4b39a2ae441c48db
Genielson <ge.nielson@hotmail.com>
2020-12-19 00:22:03 -0300

    Adicionando Bottom Navigation Bar

--- app/src/main/java/com/example/saintseiyaartsproject/home/view/HomeActivity.kt
+++ app/src/main/java/com/example/saintseiyaartsproject/home/view/HomeActivity.kt
@@ -2,23 +2,46 @@ package com.example.saintseiyaartsproject.home.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.saintseiyaartsproject.HomeFragment
+import androidx.fragment.app.Fragment
 import com.example.saintseiyaartsproject.R
+import com.example.saintseiyaartsproject.artistas.view.ArtistaFragment
+import com.example.saintseiyaartsproject.classico.view.ClassicoFragment
+import com.example.saintseiyaartsproject.omega.view.OmegaFragment
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        showFragmentScreen()
+        val homeFragment = HomeFragment()
+        val classicFragment = ClassicoFragment()
+        val omegaFragment = OmegaFragment()
+        val artistaFragment = ArtistaFragment()
+
+        showMainFragmentScreen(homeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+
+            when(it.itemId){
+                R.id.ic_home -> showMainFragmentScreen(homeFragment)
+                R.id.ic_artista -> showMainFragmentScreen(artistaFragment)
+                R.id.ic_classico -> showMainFragmentScreen(classicFragment)
+                R.id.ic_omega -> showMainFragmentScreen(omegaFragment)
+            }
+
+            true
+        }
     }
 
-    fun showFragmentScreen(){
+    fun showMainFragmentScreen(frameSection : Fragment){
 
-        var manager = supportFragmentManager
-        var transaction = manager.beginTransaction()
-        transaction.replace(R.id.frameHome,HomeFragment())
-        transaction.commit()
+        //var manager = supportFragmentManager
+        //var transaction = manager.beginTransaction()
+        //transaction.replace(R.id., frameSection)
+        //transaction.commit()
+
+        supportFragmentManager.beginTransaction().replace(R.id.flip,frameSection)
 
     }
 }
--- app/src/main/java/com/example/saintseiyaartsproject/home/view/HomeFragment.kt
+++ app/src/main/java/com/example/saintseiyaartsproject/home/view/HomeFragment.kt
@@ -1,10 +1,11 @@
-package com.example.saintseiyaartsproject
+package com.example.saintseiyaartsproject.home.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.saintseiyaartsproject.R
 
 class HomeFragment : Fragment() {
 
