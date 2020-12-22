dd7fbf01ddd220224ccd6d7314cdaa16f9826248
Genielson <ge.nielson@hotmail.com>
2020-12-21 21:44:19 -0300

    Adicionando background ceu estrelado

--- app/src/main/java/com/example/saintseiyaartsproject/home/view/HomeActivity.kt
+++ app/src/main/java/com/example/saintseiyaartsproject/home/view/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.saintseiyaartsproject.home.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.saintseiyaartsproject.R
 import com.example.saintseiyaartsproject.artistas.view.ArtistaFragment
@@ -11,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
@@ -18,7 +20,6 @@ class HomeActivity : AppCompatActivity() {
         val classicFragment = ClassicoFragment()
         val omegaFragment = OmegaFragment()
         val artistaFragment = ArtistaFragment()
-
         showMainFragmentScreen(homeFragment)
 
         bottom_navigation.setOnNavigationItemSelectedListener {
@@ -32,16 +33,17 @@ class HomeActivity : AppCompatActivity() {
 
             true
         }
+
     }
 
     fun showMainFragmentScreen(frameSection : Fragment){
 
-        //var manager = supportFragmentManager
-        //var transaction = manager.beginTransaction()
-        //transaction.replace(R.id., frameSection)
-        //transaction.commit()
+        var manager = supportFragmentManager
+        var transaction = manager.beginTransaction()
+        transaction.replace(R.id.frameHome, frameSection)
+        transaction.commit()
 
-        supportFragmentManager.beginTransaction().replace(R.id.flip,frameSection)
-
+        //supportFragmentManager.beginTransaction().replace(R.id.frameHome,frameSection)
     }
+
 }
